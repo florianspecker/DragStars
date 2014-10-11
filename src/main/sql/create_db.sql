3ee18d03cded40a37e325a1fef1f64dba333c6af
Florian Specker <florian.specker@spontacts.com>
2014-10-11 02:03:20 +0200

    - updated .gitignore - added SQL script to create tables

---
+++ src/main/sql/create_db.sql
@@ -0,0 +1,26 @@
+-- sensor_events table
+CREATE TABLE sensor_events
+(
+    id INT PRIMARY KEY,
+    type INT NOT NULL,
+    timestamp_sent BIGINT NOT NULL,
+    acc_x FLOAT NOT NULL,
+    acc_y FLOAT NOT NULL,
+    acc_z FLOAT NOT NULL,
+    gyr_x FLOAT NOT NULL,
+    gyr_y FLOAT NOT NULL,
+    gyr_z FLOAT NOT NULL,
+    mag_x FLOAT NOT NULL,
+    mag_y FLOAT NOT NULL,
+    mag_z FLOAT NOT NULL,
+    timestamp_received BIGINT NOT NULL
+);
+
+-- speed_controls table
+CREATE TABLE speed_controls
+(
+    id INT PRIMARY KEY,
+    timestamp_sent BIGINT NOT NULL,
+    timestamp_received BIGINT,
+    power DOUBLE NOT NULL
+);
