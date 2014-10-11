92b3f898edeafc79fb67bae68a2a609a5bd022bc
Florian Specker <florian.specker@spontacts.com>
2014-10-11 03:27:37 +0200

    - added comment

--- src/main/java/com/zuehlke/carrera/bot/dao/SensorEventDAO.java
+++ src/main/java/com/zuehlke/carrera/bot/dao/SensorEventDAO.java
@@ -5,7 +5,7 @@ import com.zuehlke.carrera.bot.model.SensorEvent;
 /**
  * Created on 11/10/14.
  * <p/>
- * TODO description
+ * Interface for the SensorEvent Data Access Object
  *
  * @author Florian Specker
  */
